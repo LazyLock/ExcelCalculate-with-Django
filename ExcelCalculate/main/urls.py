bb5702bd405b07944a86845b97517558ef9232b1
LazyLock <a01058121097@gmail.com>
2021-12-31 22:51:47 +0900

    파일 업로드 및 분석 기능 구현
    - pandas 라이브러리 사용

--- ExcelCalculate/main/urls.py
+++ ExcelCalculate/main/urls.py
@@ -5,8 +5,11 @@ urlpatterns = [
     path('', views.index, name='main_index'),
     path('signup', views.signup, name='main_signup'),
     path('signup/join', views.join, name='main_join'),
-    path('signin', views.signin, name='main_siginin'),
+    path('signin', views.signin, name='main_signin'),
+    path('signin/login', views.login, name='main_login'),
     path('verifyCode', views.verifyCode, name='main_verifyCode'),
     path('verify', views.verify, name='main_verify'),
-    path('result', views.result, name='main_result')
+    path('result', views.result, name='main_result'),
+    path('logout', views.logout, name='main_logout'),
+    path('calculate', views.calculate, name='main_calculate')
 ]
